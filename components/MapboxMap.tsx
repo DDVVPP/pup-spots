62ab9703dbcf3762952a17456b08fa4926aea703
DDVVPP <darshinvp@gmail.com>
2025-04-23 10:42:37 -0700

    feat: open modal on map click using ShadCN Dialog

--- components/MapboxMap.tsx
+++ components/MapboxMap.tsx
@@ -1,7 +1,14 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 import MapHeader from "./MapHeader";
 
@@ -9,13 +16,20 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
 const MapboxMap = () => {
   const mapContainerRef = useRef(null);
+  const [showModal, setShowModal] = useState(false);
 
+  // Initialize map instance
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
       style: "mapbox://styles/dvanparijs/cm9rywpj900ht01rmfnwk0y30",
       center: [-118.2437, 34.0522], // Los Angeles
       zoom: 10,
+    });
+
+    map.on("click", (e) => {
+      setShowModal((showModal) => !showModal);
+      console.log("ive been clicked", e.lngLat);
     });
 
     return () => map.remove();
@@ -25,6 +39,18 @@ const MapboxMap = () => {
     <section className="absolute inset-0 overflow-hidden ">
       <div ref={mapContainerRef} className="w-full h-screen" />
       <MapHeader />
+
+      <Dialog open={showModal} onOpenChange={setShowModal}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </section>
   );
 };
