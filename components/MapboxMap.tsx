d163f1da0af557c73862d26dec019a76046ce819
DDVVPP <darshinvp@gmail.com>
2025-04-23 12:36:17 -0700

    refactor: extracted adding marker and popups to its own file

--- components/MapboxMap.tsx
+++ components/MapboxMap.tsx
@@ -6,7 +6,8 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import MapHeader from "./MapHeader";
 import AddPinDialog from "./AddPinDialog";
 import { Coords, Pin } from "@/lib/types";
-import { preloadedPins } from "@/lib/preloadedPins";
+import { preloadedPins } from "@/lib/constants";
+import { addMarker } from "@/lib/addMarkerAndPopup";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
@@ -37,37 +38,14 @@ const MapboxMap = () => {
     return () => map.remove();
   }, []);
 
+  // Add markers and popups
   useEffect(() => {
-    if (!mapRef.current) return;
-
-    pins.forEach((pin) => {
-      const marker = new mapboxgl.Marker({ color: "#F87171" })
-        .setLngLat([pin.lng, pin.lat])
-        .addTo(mapRef.current!);
-
-      const popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        offset: 25,
-      }).setHTML(`
-      <div class="text-xs w-[200px] flex flex-col gap-y-1">
-        <p class="font-bold text-slate-800">${pin.title}</p>
-        <p class="text-slate-600 text-wrap">${pin.description}</p>
-      </div>
-    `);
-
-      // Show popup on hover
-      marker.getElement().addEventListener("mouseenter", () => {
-        popup.setLngLat([pin.lng, pin.lat]).addTo(mapRef.current!);
-      });
-
-      marker.getElement().addEventListener("mouseleave", () => {
-        popup.remove();
-      });
-    });
+    const map = mapRef.current;
+    if (!map) return;
+    pins.forEach((pin) => addMarker(pin, map));
   }, [pins]);
 
-  //Load pins from localStorage
+  // Load pins from localStorage
   useEffect(() => {
     const stored = localStorage.getItem("pup-spots");
     if (stored) {
@@ -77,12 +55,12 @@ const MapboxMap = () => {
         console.error("Failed to parse stored pins:", err);
       }
     } else {
-      // Seed pre-loaded pins on first visit
       localStorage.setItem("pup-spots", JSON.stringify(preloadedPins));
       setPins(preloadedPins);
     }
   }, []);
 
+  // Update localStorage when a pin gets added
   useEffect(() => {
     localStorage.setItem("pup-spots", JSON.stringify(pins));
   }, [pins]);
---
+++ lib/addMarkerAndPopup.tsx
@@ -0,0 +1,27 @@
+import mapboxgl from "mapbox-gl";
+import { Pin } from "./types";
+
+export function addMarker(pin: Pin, map: mapboxgl.Map) {
+  const marker = new mapboxgl.Marker({ color: "#F87171" })
+    .setLngLat([pin.lng, pin.lat])
+    .addTo(map);
+
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+    offset: 25,
+  }).setHTML(`
+    <div class="text-xs w-[200px] flex flex-col gap-y-1">
+      <p class="font-bold text-slate-800">${pin.title}</p>
+      <p class="text-slate-600 text-wrap">${pin.description}</p>
+    </div>
+  `);
+
+  marker.getElement().addEventListener("mouseenter", () => {
+    popup.setLngLat([pin.lng, pin.lat]).addTo(map);
+  });
+
+  marker.getElement().addEventListener("mouseleave", () => {
+    popup.remove();
+  });
+}
